ffa1d4ba34663803ae8f4c1daffc0981414eae7a
ElianaNeto <eliananeto29@gmail.com>
2021-07-07 21:33:11 +0100

    Novas estrategias para o bot

---
+++ api1.js
@@ -0,0 +1,41 @@
+const axios = require('axios');
+
+const ENDPOINT_API = 'http://192.168.0.29:3001/balance/2637'; //balanco da conta
+//const ENDPOINT_API = 'http://192.168.0.29:3001/exchange'; //balanco da exchange
+
+
+
+class MercadoBitcoin {
+
+    constructor(config){
+        this.config = {
+            CURRENCY: config.currency
+        }
+    }
+
+    balances() {
+        return this.call('balances');
+    }
+ 
+    async call(method){
+
+        const config = {
+            headers: {
+                'Accept': 'application/json'
+            }
+        }
+
+        try {
+            const response = await axios.get(ENDPOINT_API)
+            return response.data;
+        } 
+        catch (error) {
+            console.error(error);
+            return false;
+        }
+    }
+}
+
+module.exports = {
+    MercadoBitcoin
+}
---
+++ index1.js
@@ -0,0 +1,13 @@
+require('dotenv-safe').config();
+const {MercadoBitcoin} = require('./api1');
+
+const infoApi = new MercadoBitcoin({currency: 'ETH' });
+
+setInterval(async () => {
+    const response = await infoApi.balances();
+    console.log(response);
+   /* if(response.balances.ucana > 197000)
+        return console.log('Ta caro, aguardar')
+    else
+        return console.log('Ta barato, comprar')*/
+}, process.env.CRAWLER_INTERVAL)
