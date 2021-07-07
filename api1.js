80b5f43c66c11ae1931881954978bf7de7f3ca24
ElianaNeto <eliananeto29@gmail.com>
2021-07-07 21:36:33 +0100

    Novas estrategias para o bot para UCAN Exchange

--- api1.js
+++ api1.js
@@ -2,6 +2,8 @@ const axios = require('axios');
 
 const ENDPOINT_API = 'http://192.168.0.29:3001/balance/2637'; //balanco da conta
 //const ENDPOINT_API = 'http://192.168.0.29:3001/exchange'; //balanco da exchange
+//const ENDPOINT_TRAPI = 'http://192.168.0.29:3001/swap'; //Para o swap
+
 
 
 
